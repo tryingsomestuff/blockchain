2180c8fb98988b77ae1c79028f9df08469596226
Vivien <xr_a_y@yahoo.fr>
2022-03-03 20:44:00 +0100

    comments

--- Block.h
+++ Block.h
@@ -4,12 +4,29 @@
 #include <chrono>
 #include <string>
 /*
-Un bloc est simplement une structure contenant son numéro de bloc (index_), de la donnée (data_), un numéro arbitraire appelé NONCE diminutif de Number used ONCE (nonce_), et le hash de toutes ces données (hash_) ainsi que le hash du block précédent (prevHash_). 
-Il existe différentes règles de validation d’un bloc selon la blockchain considérée. La plupart des blockchains utilisent une preuve de travail qui consiste a prouver qu’un travail de validation a été effectuée sur le bloc. Par exemple, une règle de validation très utilisée est que le hash doit commencer par un certain nombre de 0. Le bloc va donc devoir être miné, c’est-à-dire qu’il va falloir trouver un NONCE tel que le hash du bloc commence par exemple par '000000'. Notez que ce choix est totalement arbitraire. Les concepteurs d’une blockchain peuvent par exemple choisir que le hash doive commencer par 123 ou que le hash en base 10 soit inférieur à 1000 (cas de l’Ethereum). Il faut simplement une règle qui prouve qu’un travail de validation a été effectué sur le bloc. Quand la règle est validée, le bloc est alors signé.
+Un bloc est simplement une structure contenant son numéro de bloc (index_), de la donnée (data_), 
+un numéro arbitraire appelé NONCE diminutif de Number used ONCE (nonce_), et le hash de toutes 
+ces données (hash_) ainsi que le hash du block précédent (prevHash_). 
+Il existe différentes règles de validation d’un bloc selon la blockchain considérée. La plupart 
+des blockchains utilisent une preuve de travail qui consiste a prouver qu’un travail de validation 
+a été effectuée sur le bloc. Par exemple, une règle de validation très utilisée est que le hash doit 
+commencer par un certain nombre de 0. Le bloc va donc devoir être miné, c’est-à-dire qu’il va falloir
+ trouver un NONCE tel que le hash du bloc commence par exemple par '000000'. Notez que ce choix est 
+ totalement arbitraire. Les concepteurs d’une blockchain peuvent par exemple choisir que le hash doive 
+ commencer par 123 ou que le hash en base 10 soit inférieur à 1000 (cas de l’Ethereum). Il faut 
+ simplement une règle qui prouve qu’un travail de validation a été effectué sur le bloc. Quand la 
+ règle est validée, le bloc est alors signé.
 
-Dans la fonction mine(), nous allons simplement incrémenter NONCE et recalculer le hash jusqu’à ce que l’on obtienne un hash commençant par 000000. Ce processus n’est pas fixe en temps, il peut être très court ou très long, car chaque essai de NONCE est équiprobable. Une fois cela fait, le hash est valide, le bloc est maintenant signé. Miner consiste donc à calculer le hash encore et encore jusqu’à ce que la règle de validation soit validée. 
+Dans la fonction mine(), nous allons simplement incrémenter NONCE et recalculer le hash jusqu’à ce
+que l’on obtienne un hash commençant par 000000. Ce processus n’est pas fixe en temps, il peut être 
+très court ou très long, car chaque essai de NONCE est équiprobable. Une fois cela fait, le hash est 
+valide, le bloc est maintenant signé. Miner consiste donc à calculer le hash encore et encore jusqu’à 
+ce que la règle de validation soit validée. 
 
-Si l’on tente de modifier quoi que ce soit dans un bloc signé, il perd la validité de sa signature, car le hash change et il faudrait de nouveau miner le bloc pour trouver un nouveau hash commençant par 000000. Si le bloc est déjà dans une chaine il faudrait miner à nouveau tous les blocs après ce bloc modifié.
+Si l’on tente de modifier quoi que ce soit dans un bloc signé, il perd la validité de sa signature, 
+car le hash change et il faudrait de nouveau miner le bloc pour trouver un nouveau hash commençant 
+par 000000. Si le bloc est déjà dans une chaine il faudrait miner à nouveau tous les blocs après ce 
+bloc modifié.
 */
 class Block {
   public:
--- BlockChain.h
+++ BlockChain.h
@@ -6,11 +6,17 @@
 #include "Block.h"
 
 /*
-Une blockchain simplement d’une liste de blocs pour lesquels chaque bloc contient une référence au bloc précédent via le hash. Ils sont donc "chaînées".
+Une blockchain simplement d’une liste de blocs pour lesquels chaque bloc contient une référence 
+au bloc précédent via le hash. Ils sont donc "chaînées".
 
-On comprend donc que si on change quoi que ce soit dans un bloc, ce bloc et tous les blocs suivants perdent leur signature. Il est donc très facile de savoir si la moindre information d’une blockchain a été changée et dans quel bloc.
+On comprend donc que si on change quoi que ce soit dans un bloc, ce bloc et tous les blocs suivants 
+perdent leur signature. Il est donc très facile de savoir si la moindre information d’une blockchain 
+a été changée et dans quel bloc.
 
-Il est cependant possible de miner à nouveau ces blocs non signés depuis le dernier bloc signé et revalider l’ensemble de la chaîne. Mais alors, si on peut resigner les blocs, peut-on altérer une blockchain? Non, déjà parce que miner un bloc est très demandeur en puissance de calcul et d’autant que plus vous remontez dans le temps et modifiez un bloc ancien et plus vous aurez de blocs à miner.
+Il est cependant possible de miner à nouveau ces blocs non signés depuis le dernier bloc signé et 
+revalider l’ensemble de la chaîne. Mais alors, si on peut resigner les blocs, peut-on altérer une 
+blockchain? Non, déjà parce que miner un bloc est très demandeur en puissance de calcul et d’autant 
+que plus vous remontez dans le temps et modifiez un bloc ancien et plus vous aurez de blocs à miner.
 
 Pour les cryto-monnaies c’est surtout grâce à son aspect distribué que la blockchain est véritablement sûre.
 */
